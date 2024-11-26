4da5e63237609403fcb7bc07ead06c35f0c5b466
Spudgun <jared.rj.white@gmail.com>
2024-11-26 20:23:48 +0000

    feat: exercises 1 & 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+	if (num < 0) {
+		return 'ERROR';
+	}
+	let newString = '';
+	for (i = 0; i < num; i++) {
+		newString = newString + text;
+	}
+	return newString;
 };
 
 // Do not edit below this line
