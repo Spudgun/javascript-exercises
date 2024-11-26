59eed40acb3346abe3ca72177acd9d1bf34e0cdc
Spudgun <jared.rj.white@gmail.com>
2024-11-26 21:37:59 +0000

    structure of the removeFromArray function done, except arguments.includes doesn't work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray) {
+	console.log(typeOf(arguments));
+	let _tempArray = [];
+	inputArray.forEach((element) => {
+		if (!args.includes(element)) {
+			_tempArray.push(element);
+		}
+	});
+
+	return _tempArray;
 };
 
 // Do not edit below this line
