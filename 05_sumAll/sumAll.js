5cc11967c8c6ca6e46c712ca0ebe3fa6a0ec0c6c
Spudgun <jared.rj.white@gmail.com>
2024-11-26 22:11:18 +0000

    exercise 5: complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+		return 'ERROR';
+	}
 
+	let min = num1 < num2 ? num1 : num2;
+	let max = num1 > num2 ? num1 : num2;
+
+	let sum = min;
+	for (i = min; i < max; i++) {
+		sum += i + 1;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
