e202d6819af5182d3fd3834b45dceb20c39f1a78
Spudgun <jared.rj.white@gmail.com>
2024-11-28 20:42:25 +0000

    feat: assignment 7 done - fahrenheit and celius conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+	let tempCelsius = (num - 32) * (5 / 9);
+	if (tempCelsius.toString().includes('.')) {
+		tempCelsius = Number(tempCelsius.toFixed(1));
+	}
+	return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+	let tempFahrenheit = num * (9 / 5) + 32;
+	if (tempFahrenheit.toString().includes('.')) {
+		tempFahrenheit = Number(tempFahrenheit.toFixed(1));
+	}
+	return tempFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
