f47e195764b594c031951a89190381a2253ab205
Spudgun <jared.rj.white@gmail.com>
2024-11-30 16:24:29 +0000

    exercise 9: palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = filterString(str).toLowerCase();
+	let reversestr = str.split('').reverse().join('');
+	return reversestr == str;
 };
+
+function filterString(str) {
+	return str.replace(/[^a-zA-Z0-9]/g, '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
