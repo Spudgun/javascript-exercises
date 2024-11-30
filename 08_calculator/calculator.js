34ac42cbd629c58f4e3c807936d7f1a06b07614a
Spudgun <jared.rj.white@gmail.com>
2024-11-30 16:06:56 +0000

    exercise 8: multiply test done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	return array.reduce((total, item) => {
+		return total + item;
+	});
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	return array.reduce((total, item) => {
+		return total * item;
+	});
 };
 
-const power = function() {
-	
-};
+const power = function (first, second) {};
 
-const factorial = function() {
-	
-};
+const factorial = function (first, second) {};
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
