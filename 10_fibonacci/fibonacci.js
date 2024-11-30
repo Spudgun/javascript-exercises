9475ed3a72f1d7655579e750652c28a5365f6f37
Spudgun <jared.rj.white@gmail.com>
2024-11-30 16:40:31 +0000

    feat: fabonacci numbers (head wrecking) done.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	n = parseInt(n);
+	if (n < 0) {
+		return 'OOPS';
+	} else if (n == 0) {
+		return 0;
+	} else {
+		let first = 1;
+		let second = 0;
+		for (i = 1; i < n; i++) {
+			let current = first + second;
+			second = first;
+			first = current;
+		}
+		return first;
+	}
 };
 
 // Do not edit below this line
