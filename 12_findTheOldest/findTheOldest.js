5a3a5b42d51f9b9c47e2ca8818276eca55421b42
Spudgun <jared.rj.white@gmail.com>
2024-12-01 12:13:34 +0000

    exiercise 12: findTheOldest function done for data with all dates, and also without yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	let oldestAge = 0;
+	let oldestPerson = null;
+	people.forEach((person) => {
+		person.yearOfDeath = typeof person.yearOfDeath === 'undefined' ? new Date().getFullYear() : person.yearOfDeath;
+		let personAge = person.yearOfDeath - person.yearOfBirth;
 
+		if (personAge > oldestAge) {
+			oldestAge = personAge;
+			oldestPerson = person;
+		}
+	});
+
+	return oldestPerson;
 };
 
 // Do not edit below this line
